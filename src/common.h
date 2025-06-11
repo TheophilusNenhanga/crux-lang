3f2312dfa9dd9463131af016336d5ca87496d893
TheophilusNenhanga <theonenhanga@gmail.com>
2025-06-10 22:47:23 -0600

    chore: correction

--- src/common.h
+++ src/common.h
@@ -14,7 +14,7 @@
 #define UINT8_COUNT (UINT8_MAX + 1)
 #define MAX_ARRAY_SIZE UINT16_MAX - 1
 #define FRAMES_MAX 128
-#define STACK_MAX (FRAMES_MAX * UINT8_COUNT * 8) // Approx 1MB stack size
+#define STACK_MAX (FRAMES_MAX * UINT8_COUNT * 8) // Approx 2MB stack size
 #define IMPORT_MAX FRAMES_MAX / 2
 #define RUNTIME_EXIT_CODE 70
 #define COMPILER_EXIT_CODE 65
