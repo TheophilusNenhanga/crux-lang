b8254edbf07b692b53aa3e2377637d46271a196d
Theophilus Nenhanga <theonenhanga@gmail.com>
2026-03-29 10:32:19 -0600

    feat(build): cmake build options
    - added cmake options for different debug flags
    - added variable for version number
    - added -V version number flag to cli
    - added support for stack_safety (still to come)

--- include/common.h
+++ include/common.h
@@ -11,11 +11,6 @@
 #define RED "\x1b[31m"
 #define MAGENTA "\x1b[35m"
 
-// #define DEBUG_TRACE_EXECUTION
-// #define DEBUG_PRINT_CODE
-// #define DEBUG_LOG_GC
-// #define DEBUG_STRESS_GC
-
 #define UINT8_COUNT (UINT8_MAX + 1)
 #define MAX_ARRAY_SIZE (UINT16_MAX - 1)
 #define FRAMES_MAX 128
